639fde3d2cf3b3507925d1a63fa9612212b30e02
Xuyang Liu <xliu958@wisc.edu>
2022-11-30 12:26:51 -0600

    clear type

--- Assets/Scripts/Dialogue/TypewriterEffect.cs
+++ Assets/Scripts/Dialogue/TypewriterEffect.cs
@@ -12,17 +12,20 @@ public class TypewriterEffect : MonoBehaviour {
     }
 
     private IEnumerator TypeText(string text2Type, TMP_Text label){
-        float timer = 0;
-        int charIndex = 0;
+        // float timer = 0;
+        // int charIndex = 0;
 
-        while(charIndex < text2Type.Length) {
-            timer += Time.deltaTime * typeSpeed;
-            charIndex = Mathf.FloorToInt(timer);
-            charIndex = Mathf.Clamp(charIndex, 0, text2Type.Length);
+        // while(charIndex < text2Type.Length) {
+        //     timer += Time.deltaTime * typeSpeed;
+        //     charIndex = Mathf.FloorToInt(timer);
+        //     charIndex = Mathf.Clamp(charIndex, 0, text2Type.Length);
 
-            label.text = text2Type.Substring(0, charIndex);
+        //     label.text = text2Type.Substring(0, charIndex);
 
-            yield return null;
-        }
+        //     yield return null;
+        // }
+
+        label.text = text2Type;
+        yield return null;
     }
 }
